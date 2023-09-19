8043ea1ecab9f5af7e712d8cf307dda1d8625cbc
Roshith-KT <roshithkt@gmail.com>
2023-09-20 00:18:28 +0530

    basic template styling done

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-if __name__=='__main__':
-    print("Hello Word")
+# save this as app.py
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('home.html')
+
+
+if __name__=="__main__":
+    app.run()
