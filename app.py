e399cd45c1f0b06929fab2de9338c0e84bbe87fd
Roshith-KT <roshithkt@gmail.com>
2023-09-23 20:57:34 +0530

    submit application functionality and confirm page for each job added

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # save this as app.py
-from flask import Flask,render_template,jsonify
-from database import fetch_jobs
+from flask import Flask,render_template,jsonify,request
+from database import fetch_jobs,fetch_job
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def home():
     jobs=fetch_jobs()
     return render_template('home.html',jobs=jobs)
 
@@ -14,6 +14,17 @@ def hello():
 def jobs_list():
     jobs=fetch_jobs()
     return jsonify(jobs)
+
+@app.route('/job/<id>')
+def get_job_detail(id):
+    job=fetch_job(id)
+    return render_template('job_detail.html',job=job)
+
+@app.route('/job/<id>/apply',methods=['post'])
+def submit_details(id):
+    data=request.form
+    job=fetch_job(id)
+    return render_template('submitted.html',job=job,application=data)
 
 
 if __name__=="__main__":
--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine,text
 
-connection_string="mysql+pymysql://<username>:<password>@localhost:3306/<dbname>?charset=utf8mb4" # replace <username> ,<password> and <dbname> with your mysql datbase username,password and databasename respectively
+connection_string="mysql+pymysql://root:admin#123@localhost:3306/careerwebsite?charset=utf8mb4" # replace <username> ,<password> and <dbname> with your mysql datbase username,password and databasename respectively
 
 engine=create_engine(connection_string,
                      connect_args={
@@ -18,4 +18,19 @@ def fetch_jobs():
             jobs.append(i._asdict())
         return jobs
 
-print(fetch_jobs())
+
+def fetch_job(id):
+    with engine.connect() as conn:
+        result=conn.execute(text("select * from jobs where id=:val").bindparams(val=id))
+        rows=result.all()
+        
+        if len(rows)==0:
+            return None
+        
+        else:
+            return rows[0]._asdict()
+        
+        
+
+
+
